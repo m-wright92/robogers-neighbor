35abca899236efc00c2983c5255601d482e2efa2
Michael Wright <m.wright92@icloud.com>
2022-04-01 15:51:48 -0700

    combine functions for 1, 2, and 3 into single function

--- js/scripts.js
+++ js/scripts.js
@@ -10,32 +10,39 @@ function range(input) {
   return numsOut;
 }
 
-function one(input) {
-  if (input.toString().indexOf('1') > -1){
-      return true
-  }else {
-      return false
-  }
-}
+// function beep(array) {
+//   let beepArray = array.map(function(number) {
+//     if (number.toString().includes(1)) {
+//       return "Beep!";
+//     }else {
+//       return number;
+//     }
+//   });
+//   return beepArray;
+// }
 
-function beep(array) {
-  let beepArray = array.map(function(number) {
-    if (number.toString().includes(1)) {
+// function boop(array) {
+//   let boopArray = array.map(function(number) {
+//     if (number.toString().includes(2)) {
+//       return "Boop!";
+//     }else {
+//       return number;
+//     }
+//   });
+//   return boopArray;
+// }
+
+function neighbor(array) {
+  let output = array.map(function(number) {
+    if (number.toString().includes(3)) {
+      return "Won't you be my neighbor?";
+    }else if (number.toString().includes(2)) {
+      return "Boop!";
+    }else if (number.toString().includes(1)) {
       return "Beep!";
     }else {
-      return number;
+      return number
     }
-  });
-  return beepArray;
-}
-
-function boop(array) {
-  let boopArray = array.map(function(number) {
-    if (number.toString().includes(2)) {
-      return "Beep!";
-    }else {
-      return number;
-    }
-  });
-  return boopArray;
+  })
+  return output;
 }
