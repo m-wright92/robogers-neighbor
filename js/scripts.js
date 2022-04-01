3b3c66144dab5298dace1eb81895e044e261a971
Michael Wright <m.wright92@icloud.com>
2022-04-01 15:25:53 -0700

    refactor beep function to return a Beep! if input contains a 1

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,29 @@
 
 
 function range(input) {
-  let numInput = parsInt(input);
+  let numInput = parseInt(input);
   let numsOut = [];
   for (let i=0; i <= numInput; i++) {
     numsOut.push(i);
   }
-  return numOut;
+  return numsOut;
 }
 
+function one(input) {
+  if (input.toString().indexOf('1') > -1){
+      return true
+  }else {
+      return false
+  }
+}
 
+function beep(array) {
+  let beepArray = array.map(function(number) {
+    if (number.toString().includes(1)) {
+      return "Beep!";
+    }else {
+      return number;
+    }
+  });
+  return beepArray;
+}
