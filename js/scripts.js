80da54a181f99863e294e40ae54fc917b9268328
Michael Wright <m.wright92@icloud.com>
2022-04-01 14:37:23 -0700

    rebuild functions from start as they dont interact as intended

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,13 @@
 // utility
-let numArray = [];
+
 
 function range(input) {
-  for (let i=0; i <= input; i++) {
-    numArray.push(i);
+  let numInput = parsInt(input);
+  let numsOut = [];
+  for (let i=0; i <= numInput; i++) {
+    numsOut.push(i);
   }
-  return numArray;
+  return numOut;
 }
 
 
-// business
-function one(input) {
-  if (input.toString().indexOf('1') > -1){
-      return true
-  }else {
-      return false
-  }
-}
-
-function two(input) {
-  if (input.toString().indexOf('2') > -1){
-      return true
-  }else {
-      return false
-  }
-}
-
-function three(input) {
-  if (input.toString().indexOf('3') > -1){
-      return true
-  }else {
-      return false
-  }
-}
-
-function neighbor(input) {
-  let neighArray = range(input);
-  let outputArray = [];
-  neighArray.forEach(function(number) {
-  	outputArray.push
-  });
-}
-
-// function neighbor(input) {
-//   let neighArray = range(input);
-//   // let midArray = [];
-//   let outputArray = "";
-//   for (i=0; i < neighArray.length; i++) {
-//     let midArray = neighArray.toString();
-//     if (three(neighArray) === true) {
-//       outputArray.push(neighArray)
-//       outputArray.replace("3", "sucess");
-//     }
-//     return outputArray;
-//   }
-// }
-
-// ui
-// $(document).ready(function() {
-  // let numArray = [];
-  
-  // function range(input) {
-  //   for (let i=0; i <= input; i++) {
-  //     numArray.push(i);
-  //   }
-  //   return numArray;
-  // };
-
-  // function neighbor(input) {
-  //   let outputArray = []
-  //   for (i=0; i < input; i++) {
-  //     if (three === true) {
-  //       range(input);
-  //       outputArray.push(numArray);
-  //     }
-  //     return outputArray;
-  //   }
-  // }
-  // neighbor(range());
-
-// })
